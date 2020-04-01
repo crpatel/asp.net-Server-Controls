f5ccdd64a2f9d607d30dfad3f181fc3ed32f1538
Chirag Patel <chiragrpatel@live.com>
2020-04-01 22:29:08 +0530

    Add project files.

---
+++ frmDropDown.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmDropDown.aspx.cs" Inherits="DropDownList.frmDropDown" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Drop Down List from Database<br />
+            <asp:DropDownList ID="DropDownList1" DataValueField="pkCity" DataTextField="CityName" runat="server">
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ frmDropDown.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDownList
+{
+    public partial class frmDropDown : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["DemoDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand("Select * from tblCity", conn);
+            using(conn)
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                DropDownList1.DataSource = reader;
+                //DropDownList1.DataTextField = "pkCity";
+                //DropDownList1.DataValueField = "CityName";
+                DropDownList1.DataBind();
+            }
+        }
+    }
+}
